947835b183387fcb2ef007dc43c2c96b4dc4444e
Kangjik Kim <rkqqkb@gmail.com>
2024-04-10 16:42:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price < 100000:
+        return price
+    elif price < 300000:
+        return int(price * 0.95)
+    elif price < 500000:
+        return int(price * 0.9)
+    else:
+        return int(price * 0.8)
+    return answer
