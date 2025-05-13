7fe37de1fa0f16d3c66c89e6f841444cfbdd9b22
Kangjik Kim <rkqqkb@gmail.com>
2025-05-13 17:36:05 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,18 +1,10 @@
 def solution(lines):
-    # 각 좌표가 몇 개의 선분에 포함되는지 카운트하기 위한 딕셔너리
-    line_count = {}
+    line_1 = {i for i in range(lines[0][0], lines[0][1])}
+    line_2 = {i for i in range(lines[1][0], lines[1][1])}
+    line_3 = {i for i in range(lines[2][0], lines[2][1])}
+
+    res = line_1 & line_2
+    res = res | (line_2 & line_3)
+    res = res | (line_1 & line_3)
     
-    # 모든 선분의 범위에 해당하는 좌표에 카운트 증가
-    for start, end in lines:
-        for i in range(start, end):  # end-1까지만 포함 (길이 계산은 구간으로)
-            if i not in line_count:
-                line_count[i] = 0
-            line_count[i] += 1
-    
-    # 두 개 이상의 선분이 겹치는 구간의 길이 계산
-    overlap_length = 0
-    for count in line_count.values():
-        if count >= 2:
-            overlap_length += 1
-    
-    return overlap_length
+    return len(res)
