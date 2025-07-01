fbb4c8ff2012aef88b57b6343e84bbd8ae1eb304
Kangjik Kim <rkqqkb@gmail.com>
2025-07-01 23:32:42 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.95 ms, Memory: 9.74 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -6,7 +6,7 @@ def solution(arr):
     
     if len_w > len_h:
         for i in range(len_w - len_h):
-            answer.append([0 for j in range(len_w)])
+            answer.append([0] * len_w)
     elif len_h > len_w:
         for i in range(len_h):
             for j in range(len_h - len_w):
